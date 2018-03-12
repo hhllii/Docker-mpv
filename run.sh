4b3efe4fe736791def0ba48fd2f87e2bae8c71f6
hhllii <rhhllii@qq.com>
2018-03-11 17:29:19 -0700

    sh_update

--- run.sh
+++ run.sh
@@ -2,14 +2,16 @@
 
 #!/bin/bash
 
-#(1/2/4/8 n threads) and (1/4/16/64/256 1/n instructions) totally 20 run 
+# -h for usage
+#-t (1/2/4/8 n threads) and -i (1/4/16/64/256 1/n instructions) totally 20 run 
 #set threads and instrctions for single run
+#-l run for lantency totally 50 tests
 myPath="./report/" 
 if [ ! -x "$myPath" ]; then 
 mkdir "$myPath" 
 fi 
 
-while getopts "t:i:h" arg 
+while getopts "t:i:hl" arg 
 do
         case $arg in
              t)
@@ -20,13 +22,19 @@ do
                 echo "instructions:$OPTARG" 
 		instructions=$OPTARG
                 ;;
+             l)
+		lantency=1
+                ;;
+		
              h)
                 echo "Usage:"
 		echo "-t <int> (1/2/4/8 n threads) -i <int> (1/4/16/64/256 1/n instructions)"
+		echo "-l to test the lantency"
                 ;;
              ?)
 		echo "Usage:"
             	echo "-t <int> (1/2/4/8 n threads) -i <int> (1/4/16/64/256 1/n instructions)"
+		echo "-l to test the lantency"
         exit 1
         ;;
         esac
@@ -35,6 +43,13 @@ if [ -n "$threads" ] && [ -n "$instructions" ];then
 	echo "Processing ......"
 	sudo docker run -it --privileged hhllii/mpv perf stat -e instructions,branch-misses,cycles,L1-dcache-load-misses,L1-icache-load-misses mpv qa_$instructions.mp4 --o=video_out --of=flv --ovc=libx264 --ovcopts=threads=$threads --oac=libmp3lame --oacopts=threads=$threads > ./$myPath/report_${threads}t_${instructions}i.txt
 	echo "Report at $myPath"
+elif [ -n "$lantency" ] ; then
+	echo "Processing ......"
+	for((i=0;i<50;i++));
+	do
+		sudo docker run -it --privileged hhllii/mpv perf stat -e instructions,branch-misses,cycles,L1-dcache-load-misses,L1-icache-load-misses mpv qa_test.mp4 --o=video_out --of=flv --ovc=libx264 --oac=libmp3lame >> lantency.txt
+	done
+	echo "Report at lantency.txt"
 else
 	echo "Usage:"
 	echo "-t <int> (1/2/4/8 n threads) -i <int> (1/4/16/64/256 1/n instructions)"
--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
+#clone the Dockerfile from my github
+git clone https://github.com/hhllii/Docker-mpv.git
+#create the docker locally
+sudo docker build -t local/mpv .
 #pull my docker image from docker hub 
 sudo docker pull hhllii/mpv
