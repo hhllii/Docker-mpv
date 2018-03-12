3e1829cfc0000e7dee5d1ab3f464b370724c9573
hhllii <rhhllii@qq.com>
2018-03-11 17:48:12 -0700

    cd

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #clone the Dockerfile from my github
 git clone https://github.com/hhllii/Docker-mpv.git
+#go to the dockerfile
+cd Docker-mpv
 #create the docker locally
 sudo docker build -t local/mpv .
 #pull my docker image from docker hub 
